a7c96ffcf533d114a1ccd4704a089866227d5552
RealLordzombieboy <77300730+RealLordzombieboy@users.noreply.github.com>
2026-01-13 15:26:17 -0500

    data_conversion now in a callable format
    Removed testing vehicleupdates.pb; made data_conversion.py based around the get_current_data() function to get current transport data.

--- data_conversion.py
+++ data_conversion.py
@@ -1,11 +1,8 @@
-with open("vehicleupdates.pb", "rb") as f:
-    binary_data = f.read()
-
 from google.transit import gtfs_realtime_pb2
 import requests
 
 feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get("https://api.cityofkingston.ca/gtfs-realtime/vehicleupdates.pb")
-feed.ParseFromString(response.content)
-for entity in feed.entity:
-    print(entity)
+
+def get_current_data():
+    response = requests.get("https://api.cityofkingston.ca/gtfs-realtime/vehicleupdates.pb")
+    return feed.ParseFromString(response.content)
